44ab9061e7bae6c5b7ad201e3d3b5246c6255cfb
shalaska <yiazamat@hotmail.com>
2018-02-13 09:29:57 +0100

    Guessing

--- MonacoTest/RandomizlyModel.swift
+++ MonacoTest/RandomizlyModel.swift
@@ -3,7 +3,11 @@ import Foundation
 class RandomizlyModel {
     var number = 0
     
+    init() {
+        self.newNumber()
+    }
+    
     func newNumber() {
-        self.number = Int(arc4random_uniform(6) + 1)
+        self.number = Int(arc4random_uniform(6) + 1 )
     }
 }
